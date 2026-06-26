2fd122d82e7a5f47627070b3550db70801295cc4
Josiel <josielsillva7@gmail.com>
2026-06-25 21:43:34 -0300

    adicionando exercicio em typescript

---
+++ typescript/src/teste.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso
+console.log(multiplicar(5, 4)); // 20
+console.log(saudacao("Maria")); // Olá Maria
